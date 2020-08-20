e005c085482dcf9bbafb1f79e3d6e1d27070264d
Himanshu <himanshumaimia2594@gmail.com>
2020-08-20 10:25:08 +0530

    Updates

---
+++ precache-manifest.6c983df941b95af607d237f67d48e38b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0783f5c39dd6ccab418c95e30bbceb2e",
+    "url": "/amazon/index.html"
+  },
+  {
+    "revision": "412d1e191026b222b545",
+    "url": "/amazon/static/css/main.d3dec35e.chunk.css"
+  },
+  {
+    "revision": "cbe750708f8caa989e6d",
+    "url": "/amazon/static/js/2.f9a1910c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazon/static/js/2.f9a1910c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412d1e191026b222b545",
+    "url": "/amazon/static/js/main.4db8c2c2.chunk.js"
+  },
+  {
+    "revision": "0d334e162c48ac65e78e",
+    "url": "/amazon/static/js/runtime-main.7b187eda.js"
+  }
+]);
